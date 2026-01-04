c1635c34a9ea47fac720c32bf494e382a8bfc18e
OPM <mihirraj444@gmail.com>
2026-01-04 08:17:58 +0530

    Initial Commit

---
+++ FolderCustomizerTests/FolderCustomizerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FolderCustomizerTests.swift
+//  FolderCustomizerTests
+//
+//  Created by Mihir Aman Raj on 04/01/26.
+//
+
+import Testing
+
+struct FolderCustomizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
